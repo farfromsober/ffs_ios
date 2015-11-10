5b56208db95e00ff6d7edfe7722539f12c918f24
David Regatos <david.regatos@gmail.com>
2015-11-10 21:54:32 +0100

    Initial Commit

---
+++ farFromSoberApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  farFromSoberApp
+//
+//  Created by David Regatos on 10/11/15.
+//  Copyright © 2015 David Regatos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ farFromSoberApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  farFromSoberApp
+//
+//  Created by David Regatos on 10/11/15.
+//  Copyright © 2015 David Regatos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
