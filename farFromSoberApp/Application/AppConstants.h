e2492ce6faca9c26c01defc3ccf40437ded08427
David Regatos <david.regatos@gmail.com>
2015-11-21 12:04:35 +0100

    implemented basic UserManager;

---
+++ farFromSoberApp/Application/AppConstants.h
@@ -0,0 +1,15 @@
+//
+//  AppConstants.h
+//  farFromSoberApp
+//
+//  Created by David Regatos on 21/11/15.
+//  Copyright © 2015 David Regatos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppConstants : NSObject
+
++ (NSString *)userStoragePath;
+
+@end
---
+++ farFromSoberApp/Managers/UserManager.h
@@ -0,0 +1,22 @@
+//
+//  UserManager.h
+//  farFromSoberApp
+//
+//  Created by David Regatos on 21/11/15.
+//  Copyright © 2015 David Regatos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "User.h"
+
+@interface UserManager : NSObject
+
++ (instancetype)sharedInstance;
+
+@property (readonly, nonatomic) User *currentUser;
+
+- (BOOL)createUser:(User *)user;
+- (BOOL)updateUser:(User *)user;
+- (void)resetUser;
+
+@end
