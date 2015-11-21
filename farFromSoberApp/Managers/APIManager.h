606d686e096be72fd1f693df430bd9f5cf5019f6
David Regatos <david.regatos@gmail.com>
2015-11-21 12:50:09 +0100

    implemented basic API manager;

---
+++ farFromSoberApp/Managers/APIManager.h
@@ -0,0 +1,27 @@
+//
+//  APIManager.h
+//  farFromSoberApp
+//
+//  Created by David Regatos on 21/11/15.
+//  Copyright © 2015 David Regatos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+@interface APIManager : NSObject
+
+@property (strong, nonatomic, readonly) AFHTTPSessionManager *sessionManager;
+@property (weak, nonatomic, readonly) AFNetworkReachabilityManager *reachabilityManager;
+
++ (id)sharedManager;
+
+- (void)startReachabilityMonitor;
+- (void)stopReachabilityMonitor;
+
+// login sample *****
+- (NSURLSessionDataTask *)logInViaEmail:(NSString *)userEmail
+                            andPassword:(NSString *)userPassword
+                                Success:(void (^)(NSURLSessionDataTask *task, NSDictionary *responseObject))success
+                                failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure;
+@end
