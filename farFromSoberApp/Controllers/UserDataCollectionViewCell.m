d9c8db32f7488ea7ecb2b01c19e5f996e457bfb0
Joan Biscarri <joan@biscarri.com>
2016-01-24 11:06:39 +0100

    Eliminado código no usado

--- farFromSoberApp/Controllers/UserDataCollectionViewCell.m
+++ farFromSoberApp/Controllers/UserDataCollectionViewCell.m
@@ -11,31 +11,7 @@
 #import <QuartzCore/QuartzCore.h>
 #import "UserManager.h"
 
-@implementation UserDataCollectionViewCell
-
-- (id)initWithFrame:(CGRect)frame {
-    
-    self = [super initWithFrame:frame];
-    
-    if (self) {
-        // Initialization code
-        NSArray *arrayOfViews = [[NSBundle mainBundle] loadNibNamed:@"UserDataCollectionViewCell" owner:self options:nil];
-        
-        if ([arrayOfViews count] < 1) {
-            return nil;
-        }
-        
-        if (![[arrayOfViews objectAtIndex:0] isKindOfClass:[UICollectionViewCell class]]) {
-            return nil;
-        }
-        
-        self = [arrayOfViews objectAtIndex:0];
-    }
-    
-    return self;
-    
-}
- 
+@implementation UserDataCollectionViewCell 
 
 - (void)awakeFromNib {
     // Initialization code
